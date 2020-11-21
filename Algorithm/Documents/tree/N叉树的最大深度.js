6a1419a9fd74785e8805330fd50768ea1c436e33
kelin <2312677078@qq.com>
2020-11-21 17:47:03 +0800

    complete N叉树的最大深度

--- Algorithm/Documents/tree/N叉树的最大深度.js
+++ Algorithm/Documents/tree/N叉树的最大深度.js
@@ -4,6 +4,39 @@
  * @param root
  */
 
-function maxDepth(root) {
-  
+function Node(val, children) {
+  this.val = val;
+  this.children = children;
 }
+
+function maxDepth(root) {
+  if (!root) {
+    return 0;
+  }
+  let stack = [{ key: root, value: 1 }];
+  let depth = 1;
+  while (stack.length > 0) {
+    let current = stack.shift();
+    let node = current.key;
+    let currentDepth = current.value;
+    let children = node.children || [];
+    depth = Math.max(depth, currentDepth);
+    for (let i = 0; i < children.length; i++) {
+      const element = children[i];
+      stack.push({ key: element, value: currentDepth + 1 });
+    }
+  }
+  return depth;
+}
+
+const root = new Node(1, [
+  new Node(2, [
+    new Node(3, null),
+    new Node(5, [
+      new Node(6, null)
+    ])
+  ]),
+  new Node(4, null)
+]);
+
+console.log(maxDepth(root));
